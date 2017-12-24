4cc1cb88679a6a5cf3400cfb3904f80f0485facc
linson <linson0116@163.com>
2017-12-24 15:20:57 +0800

    服务器IP修改

--- app/src/main/java/com/example/linson/notepad/ConstantUtils.java
+++ app/src/main/java/com/example/linson/notepad/ConstantUtils.java
@@ -6,11 +6,11 @@ package com.example.linson.notepad;
 
 public class ConstantUtils {
     public static final String LOCAL_UP_FILE_URL = "http://192.168.11.171:8080/MyWeb/ServletUpFile";
-    public static final String REMOTE_UP_FILE_URL = "http://123.56.111.226:8080/MyWeb/ServletUpFile";
+    public static final String REMOTE_UP_FILE_URL = "http://140.143.98.143:8080/MyWeb/ServletUpFile";
     public static final String SERVER_UP_FILE_URL = REMOTE_UP_FILE_URL;
 
     public static final String LOCAL_DOWN_FILE_URL = "http://192.168.11.171:8080/MyWeb/ServletDownFile";
-    public static final String REMOTE_DOWN_FILE_URL = "http://123.56.111.226:8080/MyWeb/ServletDownFile";
+    public static final String REMOTE_DOWN_FILE_URL = "http://140.143.98.143:8080/MyWeb/ServletDownFile";
     public static final String SERVER_DOWN_FILE_URL = REMOTE_DOWN_FILE_URL;
 
     public static final String FILE_NAME = "save.xml";
